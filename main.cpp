8f184c087d79e37726c77f6bc4fc3572cdf6d686
franccium <bawercx@gmail.com>
2026-05-08 10:11:50 +0200

    aaa

--- main.cpp
+++ main.cpp
@@ -178,7 +178,9 @@ float AcceptOfferAndGiveFuel(int auctioneer_id, float fuel_amount_proposed)
 		frame.proposed_fueld_amount = my_vehicle->state.amount_of_fuel;
 	}
 
-	my_vehicle->state.amount_of_fuel -= frame.proposed_fueld_amount;
+	TransferSending(frame.iID_receiver, FUEL, frame.proposed_fueld_amount);
+
+	//my_vehicle->state.amount_of_fuel -= frame.proposed_fueld_amount;
 	sprintf(par_view.inscription2, "Przekazanie_paliwa_w_ilosci_%f_na_rzecz_ID_%d", frame.proposed_fueld_amount, frame.iID_receiver);
 
 	int iRozmiar = multi_send->send((char*)&frame, sizeof(Frame));
@@ -373,6 +375,11 @@ DWORD WINAPI ReceiveThreadFunction(void* ptr)
 				active_auction.money_amount = frame.transfer_value;
 				active_auction.player_whos_selling = frame.iID;
 				active_auction.player_whos_buying = -1;
+
+				if (frame.remaining_auction_time < 0) {
+
+					has_active_auction = false;
+				}
 			}
 		}
 
